a7bbb3ca700a46f1ec91de5ab4719ff8c4f38195
sahin404 <sahinraj20@gmail.com>
2024-08-08 19:12:36 +0600

    environment setup

---
+++ src/routers/Routers.jsx
@@ -0,0 +1,16 @@
+import { createBrowserRouter } from "react-router-dom";
+import Home from "../pages/Home/Home";
+import Root from "../layout/Root";
+
+export const router = createBrowserRouter([
+    {
+        path:'/',
+        element:<Root></Root>,
+        children:[
+            {
+                path:'/',
+                element:<Home></Home>
+            }
+        ]
+    }
+])
